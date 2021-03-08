b0509a44079c88859b0d09c903ffe3baeaaed127
ianmcnicholas <ianjmcnicholas@gmail.com>
2021-03-08 17:17:22 +0000

    test for fizzbuzz added

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -14,11 +14,17 @@ describe('fizzBuzz', function() {
   it('returns "Fizz" when the number is divisible by 3', function() {
     expect(fizzBuzz(12)).toBe('Fizz')
     expect(fizzBuzz(9)).toBe('Fizz')
-  })
+  });
 
   it('returns "Buzz" when the number is divisible by 5', function() {
     expect(fizzBuzz(5)).toBe('Buzz')
     expect(fizzBuzz(10)).toBe('Buzz')
     expect(fizzBuzz(20)).toBe('Buzz')
+  });
+
+  it('returns "FizzBuzz" when the number is divisible by 3 and 5', function() {
+    expect(fizzBuzz(15)).toBe('FizzBuzz')
+    expect(fizzBuzz(30)).toBe('FizzBuzz')
+    expect(fizzBuzz(60)).toBe('FizzBuzz')
   })
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -13,6 +13,8 @@
 let fizzBuzz = (number) => {
     if(number % 3 === 0) {
       return 'Fizz'
+    } else if(number % 5 ===0) {
+      return 'Buzz'
     } else {
       return number
     };
